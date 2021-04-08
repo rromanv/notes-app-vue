db7b3ace356eec827cd71125d78358c40788923b
Rodolfo Roman <rromanv@gmail.com>
2021-04-07 20:21:48 -0500

    Added Form

--- src/components/NoteAdd.vue
+++ src/components/NoteAdd.vue
@@ -3,9 +3,14 @@
     class="bg-yellow-400 text-yellow-800 rounded space-y-2 flex justify-center items-center"
   >
     <button
+      @click="showToggle"
       class="opacity-40 transform transition-all hover:scale-110 hover:opacity-60"
     >
       <ic:baseline-add-circle-outline class="w-40 h-40 40" />
     </button>
   </div>
 </template>
+
+<script setup>
+import { showToggle } from '~/helpers/useNotes'
+</script>
---
+++ src/components/NotesForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="absolute inset-0 w-screen h-screen bg-purple-800 flex justify-center items-center"
+  >
+    <button @click="showToggle" class="text-white absolute top-2 left-2">
+      <ant-design:close-circle-outlined class="text-6xl" />
+    </button>
+    <form @submit.prevent class="space-y-4">
+      <div class="flex flex-col">
+        <label for="title" class="text-white mb-2">Title</label>
+        <input
+          type="text"
+          name="title"
+          class="rounded py-2 px-4"
+          placeholder="Title"
+        />
+      </div>
+      <div class="flex flex-col">
+        <label for="content" class="text-white mb-2">Content</label>
+        <textarea
+          name="content"
+          class="rounded py-2 px-4"
+          placeholder="Note Content"
+        />
+      </div>
+      <div>
+        <button
+          class="w-full bg-yellow-500 text-yellow-800 p-2 rounded mt-4 hover:bg-yellow-600 hover:text-yellow-900"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { showToggle } from '~/helpers/useNotes'
+</script>
