c8d19ee5ddbec1ee5be83864860b3c73b6ae6b28
Rodolfo Roman <rromanv@gmail.com>
2021-04-07 20:00:27 -0500

    Added Notes Design

---
+++ src/components/Note.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-yellow-400 text-yellow-800 rounded text-center p-2 space-y-2">
+    <h3 class="text-2xl font-thin tracking-wide">Note Title</h3>
+    <p class="text-sm font-medium text-yellow-900">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sapiente
+      architecto perspiciatis laudantium, quasi quam error quo veniam, odit
+      voluptatibus explicabo. Dolores cumque atque voluptate! Rem ea vel illo
+      laborum?
+    </p>
+    <div class="flex justify-end space-x-2">
+      <button class="hover:text-green-800"><mdi:playlist-edit /></button>
+      <button class="hover:text-red-800"><raphael:trash /></button>
+    </div>
+  </div>
+</template>
---
+++ src/components/NoteAdd.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="bg-yellow-400 text-yellow-800 rounded space-y-2 flex justify-center items-center"
+  >
+    <button
+      class="opacity-40 transform transition-all hover:scale-110 hover:opacity-60"
+    >
+      <ic:baseline-add-circle-outline class="w-40 h-40 40" />
+    </button>
+  </div>
+</template>
